43812ed44e8c49d2e8dc3c26582201234af54b40
Ben Smithers <ben.smithers@bristol.ac.uk>
2013-06-10 22:40:04 +0100

    Untested changes: - Moved implementation down one stage to _do_query, which provides a cleaner implementation. - Added support for the 'acceptable age' of a cached query.

--- CachingMySQLdb.py
+++ CachingMySQLdb.py
@@ -6,17 +6,30 @@ import cPickle as pickle
 
 class CachingCursorMixIn(MySQLdb.cursors.CursorStoreResultMixIn):
 
-    def _query(self, q):
+    def execute(self, query, args=None, cache=True, acceptable_age=None):
+        """Execute the given query using the parent class. Override
+        operation of caching or set a specific timeout for this query only"""
+        self.do_cache = cache
+        default_acceptable_age = self.acceptable_age
+        if acceptable_age is not None:
+            self.acceptable_age = acceptable_age
+
+        retval = super(CachingCursorMixIn, self).execute(query, args)
+
+        #Reset parameters
+        self.do_cache = True
+        self.acceptable_age = default_acceptable_age
+
+        return retval
+
+    def _do_query(self, q):
         """Either queries the database or reads results from a previously
-        cached result set"""
+        cache result set"""
 
-        #We only read the results from the cache file if: this is a 
-        #select query, and file with correct md5 exists, and file 
-        #contains correct query string (this guards against any md5 
-        #collisions)
+        if not self.do_cache or q.strip().lower()[:6] != 'select':
+            return super(CachingCursorMixIn, self)._do_query(q)
 
-        if q.strip().lower()[:6] != 'select':
-            return super(CachingCursorMixIn, self)._query(q)
+        self._last_executed = q
 
         md5 = hashlib.md5(q).hexdigest()
         cache_file = os.path.join(self.storage_dir, md5 + '.txt')
@@ -25,25 +38,40 @@ class CachingCursorMixIn(MySQLdb.cursors.CursorStoreResultMixIn):
             handle = open(cache_file, 'r')
             cached_results = pickle.load(handle)
             handle.close()
-            if cached_results["query"]  == q:
+            if cached_results["query"]  == q and (self.acceptable_age == 0 or time.time() - cached_results["timestamp"] <= self.acceptable_age):
                 print "Read query results from:", cache_file
-                self._rows = cached_results["rows"]
-                self._result = None
-                return len(self._rows)
+                # Set all relevant variables - those set inside _do_get_result
+                self._result = cached_results["result"]
+                self.rowcount = cached_results["rowcount"]
+                self.rownumber = cached_results["rownumber"]
+                self.description = cached_results["description"]
+                self.description_flags = cached_results["description_flags"]
+                self.lastrowid = cached_results["lastrowid"]
+                self._warnings = cached_results["warnings"]
+                self._info = cached_results["info"]
+                return self.rowcount
 
-        #Unable to get a cached result, so run query
-        rowcount = self._do_query(q)
-        self._rows = self._fetch_row(0)
-        self._result = None
+        #Unable to get a cached result, so run query via parent
+        retval = super(CachingCursorMixIn, self)._do_query(q)
 
         #Before returning, cache results for next time
-        cached_results = {"rows" : self._rows, "query" : q}
+        cached_results = {}
+        cached_resutls["query"] = q
+        cached_results["result"] = self._result
+        cached_results["rowcount"] = self.rowcount
+        cached_results["rownumber"] = self.rownumber
+        cached_results["description"] = self.description
+        cached_results["description_flags"] = self.description_flags
+        cached_results["lastrowid"] = self.lastrowid
+        cached_results["warnings"] = self._warnings
+        cached_results["info"] = self._info
+
         handle = open(cache_file, 'w')
         pickle.dump(cached_results, handle)
         handle.close()
         print "Saved query results to:", cache_file        
 
-        return rowcount
+        return retval
 
 class CachingCursor(CachingCursorMixIn, MySQLdb.cursors.CursorTupleRowsMixIn,
              MySQLdb.cursors.BaseCursor):
@@ -53,7 +81,7 @@ class CachingCursor(CachingCursorMixIn, MySQLdb.cursors.CursorTupleRowsMixIn,
 class CachingConnection(MySQLdb.connections.Connection):
     """Drop in for MySQLdb.connections.Connection"""
 
-    def __init__(self, storage_dir, *args, **kwargs):
+    def __init__(self, storage_dir, acceptable_age, *args, **kwargs):
         storage_dir = os.path.abspath(storage_dir)
 
         #Create storage directory if it doesn't exist. Notify user.
@@ -62,14 +90,19 @@ class CachingConnection(MySQLdb.connections.Connection):
             print "Creating directory for storage of cached result sets:", storage_dir
 
         CachingCursorMixIn.storage_dir = storage_dir
+        CachingCursorMixIn.acceptable_age = acceptable_age
+        CachingCursorMixIn.do_cache = True
+
         kwargs["cursorclass"] = CachingCursor
         super(CachingConnection, self).__init__(*args, **kwargs)
         
 
-def Connect(storage_dir, *args, **kwargs):
-    """Drop in for MySQLdb.Connect, with additional required argument
-    for storage directory of sql result sets. Directory will be created
-    if it does not exist"""
-    return CachingConnection(storage_dir, *args, **kwargs)
+def Connect(storage_dir, acceptable_age=0, *args, **kwargs):
+    """Drop in for MySQLdb.Connect, with additional arguments:
+    storage_dir: storage directory of sql result sets. Directory will be created
+    if it does not exist
+    acceptable_age: number of seconds old the cached verion is allowed to be.
+    Default = 0 = any age"""
+    return CachingConnection(storage_dir, acceptable_age, *args, **kwargs)
 
 connect = Connection = Connect
